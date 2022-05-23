232a5b23c093b340a7acfd3fd787d9fcb43d4775
7ofuuu <ilhamfir09@gmail.com>
2022-05-23 21:49:19 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
+
+function responsive() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
